ad6834aa33e2a627dae3d171a726f2f08f7c3cca
mythunlearner <learntechpassion@gmail.com>
2025-08-02 07:26:53 +0530

    Added BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import NavBar from "./NavBar"
-
+import { BrowserRouter,Routes, Route } from "react-router-dom"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
 function App() {
   
 
   return (
     <>
-   <NavBar/>
+    <BrowserRouter basename="/">
+       <Routes>
+          <Route path="/" element={<Body/>}> 
+             <Route path="/login" element={<Login/>}> </Route>
+             <Route path="/profile" element={<Profile/>}></Route> 
+          </Route>
+         
+         
+       </Routes>
+    </BrowserRouter>
+   {/* <NavBar/> */}
     </>
   )
 }
